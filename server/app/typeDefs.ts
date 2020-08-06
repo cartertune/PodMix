f948330b18549be9f1c2b91c5e6963d181fb72f5
Carter Tune <cartertune@gmail.com>
2020-08-06 12:40:49 -0500

    basic schema

--- server/app/typeDefs.ts
+++ server/app/typeDefs.ts
@@ -3,17 +3,39 @@ import { gql } from "apollo-server-lambda";
 const typeDefs = gql`
   type User {
     id: ID
-    username: String
     name: String
     avatarUrl: String
-    bio: String
-    createdPlaylists: [Playlist]
+    email: String
+    projects: [Project]
   }
+  type Project {
+    title: String
+    owner: User
+    collaborators: [User]
+    mixes: [Mix]
+  }
+  type Mix {
+    file: File
+    title: String
+    comments: [Comment]
+  }
+  type Comment {
+    time: String # may need to rethink formatting
+    text: String
+    isComplete: Boolean
+  }
+
+  #TODO: Figure out inputs to these mutations
   type Mutation {
     login: User
+    createUser: User
+    createProject: Project
+    createMix: Project
+    createComment: Project
   }
   type Query {
     user(id: ID!): User
+    project(id: ID!): Project
   }
 `;
 
