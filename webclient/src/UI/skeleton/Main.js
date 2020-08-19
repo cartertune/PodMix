651db5e0ea180c07dd25ef1419052ec89c177e0f
Carter Tune <cartertune@gmail.com>
2020-08-19 14:01:53 -0500

    minimal changes to support loggin in

--- webclient/src/UI/skeleton/Main.js
+++ webclient/src/UI/skeleton/Main.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import { compose } from "react-apollo";
 import LoginSplashPage from "../login-splash/LoginSplashPage";
+import { login } from "../../connections/userConnections";
 import AuthCallback from "../../auth/AuthCallback";
 
 const Main = (props) => {
-  // const { loginToServer } = props;
+  const { loginToServer } = props;
 
-  // if (
-  //   !_.get(store.getState(), "auth.user.id") &&
-  //   _.get(store.getState(), "auth.jwt")
-  // ) {
-  //   loginToServer().then(({ data }) => {
-  //     store.dispatch({ type: "SET_USER_ID", id: data.login.id });
-  //   });
-  // }
+  if (
+    !_.get(store.getState(), "auth.user.id") &&
+    _.get(store.getState(), "auth.jwt")
+  ) {
+    loginToServer().then(({ data }) => {
+      store.dispatch({ type: "SET_USER_ID", id: data.login.id });
+    });
+  }
 
   return (
     <main className="container main-page">
@@ -30,4 +32,4 @@ const Main = (props) => {
   );
 };
 
-export default Main;
+export default compose(login)(Main);
--- webclient/src/auth/AuthCallback.js
+++ webclient/src/auth/AuthCallback.js
@@ -5,23 +5,22 @@ import _ from "lodash";
 import { LoadingScreen } from "../UI/components/Loading";
 
 class AuthCallback extends Component {
-  //Auth.handleAuthentication(props.history);
-  // componentDidMount() {
-  //   const { history, loginToServer } = this.props;
-  //   const handleAuthCallback = () => {
-  //     loginToServer()
-  //       .then(({ data }) => {
-  //         const dispatch = _.get(store, "dispatch");
-  //         if (dispatch) {
-  //           dispatch({ type: "SET_USER_ID", id: data.login.id });
-  //         }
-  //       })
-  //       .catch(error => {
-  //         console.log(error);
-  //       });
-  //   };
-  //   Auth.handleAuthentication(history, handleAuthCallback);
-  // }
+  componentDidMount() {
+    const { history, loginToServer } = this.props;
+    const handleAuthCallback = () => {
+      loginToServer()
+        .then(({ data }) => {
+          const dispatch = _.get(store, "dispatch");
+          if (dispatch) {
+            dispatch({ type: "SET_USER_ID", id: data.login.id });
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    Auth.handleAuthentication(history, handleAuthCallback);
+  }
 
   render() {
     return <LoadingScreen />;
