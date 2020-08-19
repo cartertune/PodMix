017e5131bf17899868be9a4bf56b8ea1ce2d11cc
Carter Tune <cartertune@gmail.com>
2020-08-19 13:39:21 -0500

    login splash page

--- webclient/src/UI/login-splash/LoginSplashPage.js
+++ webclient/src/UI/login-splash/LoginSplashPage.js
@@ -1,6 +1,8 @@
 import React from "react";
+import Auth from "../../auth/Auth";
 
 const LoginSplashPage = (props) => {
+  const { currentLocation } = props;
   return (
     <div className="login-splash-page">
       <div className="logo-section row">
@@ -11,7 +13,12 @@ const LoginSplashPage = (props) => {
         <div className="col-2" />
       </div>
       <div className="bottom-section row justify-content-center mt-4">
-        <button className="btn btn-primary col-8 py-4">
+        <button
+          className="btn btn-primary col-8 py-4"
+          onClick={() => {
+            Auth.login({ callbackLink: "/" });
+          }}
+        >
           <h3>Login / Sign-up</h3>
         </button>
       </div>
