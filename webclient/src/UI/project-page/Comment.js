7d444e63ddb3122f727ee1c6f3e35ae59f04e8e1
Carter Tune <cartertune@gmail.com>
2020-10-20 14:40:58 -0500

    side swipe comments

--- webclient/src/UI/project-page/Comment.js
+++ webclient/src/UI/project-page/Comment.js
@@ -3,35 +3,36 @@ import { secondsToTimestamp } from "../../util/util";
 import Avatar from "../components/Avatar";
 import Auth from "../../auth/Auth";
 import { FiX } from "react-icons/fi";
-import Interactable from 'react-interactable/noNative'
+import Interactable from "react-interactable/noNative";
 
 
 const Comment = (props) => {
   const { comment, onClick, onDelete } = props;
   const { time, text, creator } = comment;
+
   const user = Auth.getUser();
+  const snapPoints = [{ x: -150 }, { x: 150 }];
 
   return (
-    <Interactable.View>
+  <Interactable.View horizontalOnly={true} className="w-100">
     <div className="comment position-relative">
       <div className="timestamp-container">
         <p>{`${secondsToTimestamp(time)} -`}</p>
       </div>
-      <div
-        className="form-control position-relative withtimestamp d-flex justify-content-between"
-        onClick={onClick}
-      >
-        <p>{text}</p>
+    <div
+      className="form-control position-relative withtimestamp d-flex justify-content-between"
+      onClick={onClick}
+    >
+      <p>{text}</p>
         <div className="avatar-container">
           {creator.email == _.get(user, "email") ? (
-            <FiX className="delete-button" onClick={() => onDelete()} />
-          ) : (
-            <Avatar avatarUrl={_.get(comment, "creator.avatarUrl")} />
-          )}
+            <FiX className="delete-button" onClick={() => onDelete()} />) : (
+          <Avatar avatarUrl={_.get(comment, "creator.avatarUrl")} />
+        )}
         </div>
       </div>
     </div>
-</Interactable.View>
+  </Interactable.View>
   );
 };
 
