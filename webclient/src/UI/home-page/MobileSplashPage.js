43d044c8c1d281ede672d4efe8c99ae3f3cac1c4
Carter Tune <cartertune@gmail.com>
2020-11-04 13:12:29 -0600

    mobile splash page

--- webclient/src/UI/home-page/MobileSplashPage.js
+++ webclient/src/UI/home-page/MobileSplashPage.js
@@ -1,27 +1,39 @@
 import React from "react";
-import logo from "../../resources/BigLogo.png";
+import textLogoCombo from "../../resources/TextLogoComboBig.png";
+import screenshot from "../../resources/Screenshot_iphonexspacegrey_portrait.png";
+
 import Auth from "../../auth/Auth";
 import H3 from "../components/H3";
+import P from "../components/P";
 
 const MobileSplashPage = (props) => {
   return (
-    <div className="mobile-login-splash-page">
-      <div className="logo-section row">
-        <div className="col-2" />
-        <div className="logo col-8 pt-100">
-          <img src={logo} />
+    <div className="mobile-login-splash-page row">
+      <div className="top-section">
+        <div className="logo-combo pt-3">
+          <img src={textLogoCombo} />
         </div>
-        <div className="col-2" />
+        <div className="slogan py-3">
+          <h2>Take your music seriously, not yourself</h2>
+        </div>
+        <div className="screenshot-container">
+          <img src={screenshot} />
+        </div>
+        <div className="px-2 d-flex justify-content-center my-5">
+          <button
+            className="btn btn-primary signup-button py-3 px-5"
+            onClick={() => {
+              Auth.login({ callbackLink: "/" });
+            }}
+          >
+            <H3>Sign-up for Free</H3>
+          </button>
+        </div>
       </div>
-      <div className="bottom-section row justify-content-center mt-4">
-        <button
-          className="btn btn-primary col-8 py-4"
-          onClick={() => {
-            Auth.login({ callbackLink: "/" });
-          }}
-        >
-          <H3>Login / Sign-up</H3>
-        </button>
+      <div className="detail-section col-10 py-4">
+        <P className="pb-1">- Keep your project collaboration organized</P>
+        <P className="pb-1">- Upload all your mixes to one location</P>
+        <P className="pb-1">- Keep the feedback process quick & easy </P>
       </div>
     </div>
   );
