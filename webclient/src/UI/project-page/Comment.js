02ea8938917edb75e02cc40309d5c582347253d7
Carter Tune <cartertune@gmail.com>
2020-11-11 13:30:51 -0600

    complete_comment_ui

--- webclient/src/UI/project-page/Comment.js
+++ webclient/src/UI/project-page/Comment.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { secondsToTimestamp } from "../../util/util";
 import Avatar from "../components/Avatar";
-import Auth from "../../auth/Auth";
-import { FiX } from "react-icons/fi";
+import { FiTrash, FiCheck, FiMoreVertical, FiX } from "react-icons/fi";
 import P from "../components/P";
+import Dropdown from "../components/Dropdown";
 
 const Comment = (props) => {
-  const { comment, onClick, onDelete, isHighlighted } = props;
-  const { time, text, creator } = comment;
-  const user = Auth.getUser();
+  const {
+    comment,
+    onClick,
+    onDelete,
+    onCompleteComment,
+    isHighlighted,
+  } = props;
+  const { time, text } = comment;
+
+  const [showOptions, setShowOptions] = useState(false);
 
   return (
     <div className="comment position-relative">
@@ -23,11 +30,28 @@ const Comment = (props) => {
       >
         <P>{text}</P>
         <div className="avatar-container">
-          {creator.email == _.get(user, "email") ? (
-            <FiX className="delete-button" onClick={() => onDelete()} />
-          ) : (
-            <Avatar avatarUrl={_.get(comment, "creator.avatarUrl")} />
-          )}
+          <Avatar avatarUrl={_.get(comment, "creator.avatarUrl")} />
+          <div className="pl-1 position-relative">
+            {showOptions ? (
+              <FiX onClick={() => setShowOptions(false)} />
+            ) : (
+              <FiMoreVertical onClick={() => setShowOptions(true)} />
+            )}
+            <Dropdown show={showOptions}>
+              <div onClick={() => onCompleteComment()}>
+                <div className="p-1">
+                  <FiCheck />
+                </div>
+                <P>Complete</P>
+              </div>
+              <div onClick={() => onDelete()}>
+                <div className="p-1">
+                  <FiTrash />
+                </div>
+                <P>Delete</P>
+              </div>
+            </Dropdown>
+          </div>
         </div>
       </div>
     </div>
