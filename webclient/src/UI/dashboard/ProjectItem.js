e2be2b1070a0f583843fadaefed3c91d934d6d2c
Carter Tune <cartertune@gmail.com>
2020-12-23 13:41:54 -0600

    starting to update dashboard

--- webclient/src/UI/dashboard/ProjectItem.js
+++ webclient/src/UI/dashboard/ProjectItem.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import P from "../components/P";
+import { FiChevronDown, FiChevronUp } from "react-icons/fi";
 
 const ProjectItem = (props) => {
   const { project } = props;
@@ -12,7 +13,7 @@ const ProjectItem = (props) => {
       <div className="form-control d-flex align-items-center">
         <P>{title}</P>
         <div className="position-absolute chevron-icon">
-          <FiChevronDown />
+          {isExpanded ? <FiChevronUp /> : <FiChevronDown />}
         </div>
       </div>
     </Link>
